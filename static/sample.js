ec50cab719c6d766b7f20ac270fb248f943c3d85
Mark Beasley <mbeasl1@gmail.com>
2017-02-24 14:12:17 -0800

    Plotting sample data

---
+++ static/sample.js
@@ -0,0 +1,137 @@
+var sampleData = [
+    {
+        "paper": "Michigan Daily",
+        "city": "Ann Arbor, MI",
+        "date": "2016-7-27",
+        "headlines": [
+            {
+                "title":"2016 Schefters: Several Michigan teams recover from down 2014-15 season",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+            {
+                "title":"Abby Cole's move to basketball made official",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Anna Polumbo-Levy: Prioritizing self-care",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Art Fair showcases a different side of Ann Arbor",
+                "data":{"neg": 0.0, "neu": 0.753, "pos": 0.247, "compound": 0.3182}
+            },
+
+            {
+                "title":"Elena Hubbell: Inspiring movies for all",
+                "data":{"neg": 0.0, "neu": 0.641, "pos": 0.359, "compound": 0.4215}
+            },
+
+            {
+                "title":"From Mexico to Michigan",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Heading into second year under Harbaugh, Wolverines embracing higher standard",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Jacob Rich: Pokémon Go grabbed the world by the ear — so what's next for Nintendo?",
+                "data":{"neg": 0.0, "neu": 0.806, "pos": 0.194, "compound": 0.5574}
+            },
+
+            {
+                "title":"Kildee endorses Clinton's running mate",
+                "data":{"neg": 0.0, "neu": 0.625, "pos": 0.375, "compound": 0.34}
+            },
+
+            {
+                "title":"M Den provides preview of Nike product launch",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Madeline Nowicki: Make America Work Again?",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Michigan delegation appears unified in support of Trump despite roll call confusion",
+                "data":{"neg": 0.0, "neu": 0.556, "pos": 0.444, "compound": 0.7342}
+            },
+
+            {
+                "title":"Notebook: Harbaugh, Big Ten coaches address media in Chicago",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Op-ed: Fixing the income gap",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Op-ed: Jolly to the grave",
+                "data":{"neg": 0.292, "neu": 0.337, "pos": 0.371, "compound": 0.1779}
+            },
+
+            {
+                "title":"Photos of the Year",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Photos of the Year: Entertaiment",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Photos of the Year: News",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"School of Public Health named to national precision medicine program",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Students could turn the tide with Michigan as a swing state",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Students of the Year",
+                "data":{"neg": 0.0, "neu": 1.0, "pos": 0.0, "compound": 0.0}
+            },
+
+            {
+                "title":"Young people rock DNC with protests, but students appear less involved",
+                "data":{"neg": 0.127, "neu": 0.873, "pos": 0.0, "compound": -0.1154}
+            },
+
+            {
+                "title":"‘Ballers’ fails to deliver on its bombastic promises in season premiere",
+                "data":{"neg": 0.194, "neu": 0.625, "pos": 0.181, "compound": -0.0516}
+            },
+
+            {
+                "title":"‘Captain Fantastic’ is a brave exploration of the human condition",
+                "data":{"neg": 0.0, "neu": 0.569, "pos": 0.431, "compound": 0.6486}
+            },
+
+            {
+                "title":"‘Suits’ finds itself in an identity crisis in latest season",
+                "data":{"neg": 0.313, "neu": 0.687, "pos": 0.0, "compound": -0.6249}
+            },
+
+            {
+                "title":"‘Your Authority’ shows how Good Charlotte's talent has yet to waver",
+                "data":{"neg": 0.0, "neu": 0.612, "pos": 0.388, "compound": 0.6908}
+            }
+        ]
+    }
+];
--- static/site.js
+++ static/site.js
@@ -1,6 +1,8 @@
-var map, heatmap;
+var map, negativeHeatmap, positiveHeatmap, geocoder;
 
 function initMap() {
+  geocoder = new google.maps.Geocoder();
+
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: {lat: 37.09024, lng: -95.712891},
@@ -8,8 +10,77 @@ function initMap() {
     styles: customMapStyle
   });
 
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: [],
-    map: map
+  var netMood = 0;
+  for(var i=0; i < sampleData[0].headlines.length; i++) {
+    netMood += sampleData[0].headlines[i].data.compound;
+  }
+  sampleData[0].netMood = netMood;
+  console.log(netMood);
+
+  geocoder.geocode( { address: sampleData[0].city }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        sampleData[0].location = results[0].geometry.location;
+
+        // would plot after processing all points..
+        plotData();
+    }else{
+        console.log('error: '+_stores[i].store+' status: '+status);
+    }
   });
+}
+
+
+function plotData() {
+
+  if(sampleData[0].netMood > 0) {
+    positiveHeatmap = new google.maps.visualization.HeatmapLayer({
+      data: [
+        { location: sampleData[0].location, weight: sampleData[0].netMood }
+      ],
+      map: map,
+      radius: 30,
+      gradient: [
+        'rgba(0,0,0,0)',
+        'rgba(229, 255, 229, 0.1)',
+        'rgba(229, 255, 229, 0.2)',
+        'rgba(229, 255, 229, 0.3)',
+
+        'rgba(229, 255, 229, 0.4)',
+        'rgba(204, 255, 204, 0.5)',
+        'rgba(178, 255, 178, 0.6)',
+        'rgba(153, 255, 153, 0.65)',
+        'rgba(127, 255, 127, 0.7)',
+        'rgba(102, 255, 102, 0.75)',
+        'rgba(76, 255, 76, 0.8)',
+        'rgba(50, 255, 50, 0.9)',
+        'rgba(25, 255, 25, 0.95)',
+        'rgba(0, 255, 0, 1.0)'
+      ]
+    });
+  } else {
+    negativeHeatmap = new google.maps.visualization.HeatmapLayer({
+      data: [
+        {location: sampleData[0].location, weight: sampleData[0].netMood}
+      ],
+      map: map,
+      radius: 30,
+      gradient: [
+        'rgba(0,0,0,0)',
+        'rgba(255, 248, 248, 0.1)',
+        'rgba(255, 235, 236, 0.2)',
+        'rgba(255, 233, 234, 0.3)',
+
+        'rgba(255, 229, 229, 0.4)',
+        'rgba(255, 204, 204, 0.5)',
+        'rgba(255, 178, 178, 0.6)',
+        'rgba(255, 153, 153, 0.65)',
+        'rgba(255, 127, 127, 0.7)',
+        'rgba(255, 102, 102, 0.75)',
+        'rgba(255, 76, 76, 0.8)',
+        'rgba(255, 50, 50, 0.9)',
+        'rgba(255, 25, 25, 0.95)',
+        'rgba(255, 0, 0, 1.0)'
+      ]
+    });
+  }
 }
