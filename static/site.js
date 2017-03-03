a46ebf982dee48e4ced332677e85caf12d2bb689
Lauren Ko <lauren.ko@unt.edu>
2017-03-02 18:49:56 -0600

    Get word cloud words from headlines.

--- static/site.js
+++ static/site.js
@@ -115,17 +115,22 @@ function showPoints() {
     var marker = new google.maps.Marker({
       position: sampleData[i].location,
       map: map,
+      title: sampleData[i].paper,
       animation: google.maps.Animation.DROP,
       data: sampleData[i]
     });
-
     google.maps.event.addListener(marker, 'click', (function(lmap, lmarker) {
       return function() {
-        // super quick word cloud generation
-        document.getElementById("word-cloud").innerHTML = "<img src='static/wordcloud.svg'>";
+        //Create a new instance of the word cloud visualisation.
+        var headlines = lmarker.data.headlines.map(function(newsSource) {
+    return newsSource.title;
+  });
+        var words = headlines.join(' ');
+        var myWordCloud = wordCloud(document.getElementById('word-cloud'));
+        myWordCloud.update(getWords(words));
 
         var titles = document.getElementById('titles');
-        var html = '<h3>Headlines</h3><ul>';
+        var html = '<h3>' + lmarker.data.paper + ' Headlines</h3><ul>';
         for(var k=0; k < lmarker.data.headlines.length; k++) {
           html += '<li onClick="showRelated(\'' + lmarker.data.headlines[k].title + '\');">' + lmarker.data.headlines[k].title + '</li>';
         }
@@ -147,4 +152,74 @@ function showRelated(headline) {
         document.getElementById('graphs').innerHTML = html;
       })
   });
+}
+
+// Adapted from https://gist.github.com/jwhitfieldseed/9697914
+// Encapsulate the word cloud functionality
+function wordCloud(selector) {
+    var fill = d3.scale.category20();
+    //Construct the word cloud's SVG element
+    var svg = d3.select(selector).append('svg')
+        .attr('width', 500)
+        .attr('height', 500)
+        .append('g')
+        .attr('transform', 'translate(250,250)');
+    //Draw the word cloud
+    function draw(words) {
+        var cloud = svg.selectAll("g text")
+                        .data(words, function(d) { return d.text; })
+        //Entering words
+        cloud.enter()
+            .append('text')
+            .style('font-family', 'Impact')
+            .style('fill', function(d, i) { return fill(i); })
+            .attr('text-anchor', 'middle')
+            .attr('font-size', 1)
+            .text(function(d) { return d.text; });
+        //Entering and existing words
+        cloud
+            .transition()
+                .duration(600)
+                .style('font-size', function(d) { return d.size + 'px'; })
+                .attr('transform', function(d) {
+                    return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
+                })
+                .style('fill-opacity', 1);
+        //Exiting words
+        cloud.exit()
+            .transition()
+                .duration(200)
+                .style('fill-opacity', 1e-6)
+                .attr('font-size', 1)
+                .remove();
+    }
+    //Use the module pattern to encapsulate the visualisation code. We'll
+    // expose only the parts that need to be public.
+    return {
+        //Recompute the word cloud for a new set of words. This method will
+        // asycnhronously call draw when the layout has been computed.
+        //The outside world will need to call this function, so make it part
+        // of the wordCloud return value.
+        update: function(words) {
+            d3.layout.cloud().size([500, 500])
+                .words(words)
+                .padding(5)
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font('Impact')
+                .fontSize(function(d) { return d.size; })
+                .on('end', draw)
+                .start();
+        }
+    }
+}
+
+//Prepare one of the sample sentences by removing punctuation,
+// creating an array of words and computing a random size attribute.
+function getWords(words) {
+    return words
+            .replace(/[!\.,:;\?]/g, '')
+            .split(' ')
+            .map(function(d) {
+                return {text: d, size: 10 + Math.random() * 60};
+            })
 }
